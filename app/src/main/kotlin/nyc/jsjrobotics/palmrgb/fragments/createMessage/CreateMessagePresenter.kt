4b6fbca1c2669913cf889260d8bb1c671ad52da1
Jonathan Colon <jonatron2.0@gmail.com>
2018-05-12 21:43:27 -0400

    can upload message on send clicked

--- app/src/main/kotlin/nyc/jsjrobotics/palmrgb/fragments/createMessage/CreateMessagePresenter.kt
+++ app/src/main/kotlin/nyc/jsjrobotics/palmrgb/fragments/createMessage/CreateMessagePresenter.kt
@@ -1,19 +1,26 @@
 package nyc.jsjrobotics.palmrgb.fragments.createMessage
 
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.OnLifecycleEvent
 import android.text.Editable
 import android.text.TextWatcher
+import io.reactivex.disposables.CompositeDisposable
+import nyc.jsjrobotics.palmrgb.DEBUG
 import nyc.jsjrobotics.palmrgb.androidInterfaces.DefaultPresenter
 import javax.inject.Inject
 
-class CreateMessagePresenter @Inject constructor(val model : CreateMessageModel): DefaultPresenter(){
-    private val textWatcher : TextWatcher = buildTextWatcher()
+class CreateMessagePresenter @Inject constructor(val model: CreateMessageModel) : DefaultPresenter() {
+    lateinit var view: CreateMessageView
+    private val textWatcher: TextWatcher = buildTextWatcher()
 
     fun init(view: CreateMessageView) {
+        this.view = view
         view.setInputListener(textWatcher)
+        subscribeSendClicked()
     }
 
     private fun buildTextWatcher(): TextWatcher {
-        return object : TextWatcher{
+        return object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // ignored
             }
@@ -26,5 +33,20 @@ class CreateMessagePresenter @Inject constructor(val model : CreateMessageModel)
                 model.displayText(s.toString())
             }
         }
+    }
+
+    private val disposables: CompositeDisposable = CompositeDisposable()
+
+    private fun subscribeSendClicked() {
+        DEBUG("Send button clicked...")
+        val connectDisposable = view.onSendClicked.subscribe { messageTitle ->
+            model.uploadMessageToFirebase(messageTitle)
+        }
+        disposables.add(connectDisposable)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun unsubscribe() {
+        disposables.clear()
     }
 }
--- app/src/main/kotlin/nyc/jsjrobotics/palmrgb/fragments/createMessage/CreateMessageView.kt
+++ app/src/main/kotlin/nyc/jsjrobotics/palmrgb/fragments/createMessage/CreateMessageView.kt
@@ -8,21 +8,29 @@ import android.support.constraint.ConstraintLayout
 import android.text.TextWatcher
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
+import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.PublishSubject
 import nyc.jsjrobotics.palmrgb.R
 import nyc.jsjrobotics.palmrgb.customViews.XmlDiodeArray
 import nyc.jsjrobotics.palmrgb.inflate
 import javax.inject.Inject
 
 class CreateMessageView @Inject constructor(val model: CreateMessageModel,
-                                            val largeDiodeArray: XmlDiodeArray ) : LifecycleObserver {
+                                            val largeDiodeArray: XmlDiodeArray) : LifecycleObserver {
     lateinit var rootXml: View
 
     private lateinit var largeMatrix: ConstraintLayout
     private lateinit var smallMatrix: ConstraintLayout
     private lateinit var input: EditText
+    private lateinit var sendButton: Button
     private var displayChangedDisposable: Disposable? = null
+
+    private val sendClicked: PublishSubject<String> = PublishSubject.create()
+    val onSendClicked: Observable<String> = sendClicked
+
 
     fun initView(container: ViewGroup, savedInstanceState: Bundle?) {
         rootXml = container.inflate(R.layout.fragment_create_message)
@@ -31,9 +39,22 @@ class CreateMessageView @Inject constructor(val model: CreateMessageModel,
         smallMatrix = rootXml.findViewById(R.id.rgb_matrix32)
         smallMatrix.visibility = View.GONE
         input = rootXml.findViewById(R.id.message_text)
-        displayChangedDisposable = model.onDisplayChanged.subscribe{
+        sendButton = rootXml.findViewById(R.id.send_button)
+        displayChangedDisposable = model.onDisplayChanged.subscribe {
             largeDiodeArray.showColors(model.displayedColors)
         }
+
+        setUpSendButton()
+    }
+
+    private fun setUpSendButton() {
+        sendButton.setOnClickListener {
+            val message = input.editableText.toString()
+            if (message.isNotEmpty()) {
+                sendClicked.onNext(message)
+            }
+        }
+
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
@@ -41,7 +62,7 @@ class CreateMessageView @Inject constructor(val model: CreateMessageModel,
         displayChangedDisposable?.dispose()
     }
 
-    fun setInputListener(textWatcher : TextWatcher) {
+    fun setInputListener(textWatcher: TextWatcher) {
         input.addTextChangedListener(textWatcher)
     }
 
